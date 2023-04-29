f0d87e2f8784007a6888f213e341cbc40d1d989d
Omkar <omkarrao5123@gmail.com>
2023-04-29 19:39:59 +0530

    api routes

--- devbuddy/api/urls.py
+++ devbuddy/api/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import *
+import api.views as apiviews
+import base.views as baseviews
+import register.views as registerviews
+from django.conf.urls import include
+
 
 urlpatterns = [
-    path('', getRoutes, name="routes"),
+    path('', apiviews.getRoutes, name="routes"),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('/user/profile/',apiviews.profile, name="user"),
+    path('/user/teams/',apiviews.teams, name="teams"),
+    path('/user/createteam/',apiviews.createteam, name="createteam"),
+    path('/user/logout/',registerviews.logout, name="logout"),
+
 
 ]
