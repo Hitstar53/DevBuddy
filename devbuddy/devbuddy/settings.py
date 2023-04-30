98046ff503269c9818fc406c6bd7b90ee739299e
Hatim Sawai <hatimsawai76@gmail.com>
2023-04-30 08:30:10 +0530

    csrf final
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- devbuddy/devbuddy/settings.py
+++ devbuddy/devbuddy/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = [
+    'https://devbuddy-production.up.railway.app',
+    'https://devbuddy-production.up.railway.app/',
+]
 
 
 # Application definition
